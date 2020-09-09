b6ac377e0a19b6e27ac819142c5ca0c2cc4c0e34
Kartik <kartik@hyperverge.co>
2020-09-09 13:44:19 +0530

    Add rate Limiter middleware code

---
+++ README.md
@@ -0,0 +1,5 @@
+## ratelimiter-middleware
+
+![](https://img.shields.io/badge/npm-v1.0.0-brightgreen)
+![](https://img.shields.io/badge/middleware-rate%20limit-blue)
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const rateLimiter = (maxReqCount, timeLimit, appId) => (req, res, next) => {
+  let token = null;
+  if (!appId) {
+    token = req.ip;
+  } else {
+    token = req.get(appId);
+  }
+  if (!token) {
+    return res.status(400).json({ err: 'No appId detected' });
+  }
+  client
+    .multi()
+    .set([token, 0, 'EX', timeLimit, 'NX'])
+    .incr(token)
+    .exec((err, replies) => {
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+      const reqCount = replies[1];
+      if (reqCount > maxReqCount) {
+        return res.status(403).json({ err: `Quota of ${maxReqCount} requests per ${timeLimit} seconds exceeded` });
+      }
+      return next();
+    });
+};
+
+module.exports = { rateLimiter };
